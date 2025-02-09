8b38b92434058f8f5eacc87258c43e6c3353fa29
Ilya Prikhodko <zapaska96@gmail.com>
2025-02-09 18:45:23 +0500

    отступы

--- frontend/src/components/TrailerComponent.vue
+++ frontend/src/components/TrailerComponent.vue
@@ -5,17 +5,14 @@
 <style>
 .trailer {
   display: block;
-
   width: 15px;
   height: 15px;
   border-radius: 100%;
-
   background-color: var(--clr-trailer);
   position: fixed;
   top: 0;
   left: 0;
   z-index: 100000;
-
   pointer-events: none;
 }
 </style>
